795e12b6ae92f4a8aedd316ab3b0cdceed80a6bb
Emmanuel.Arante <arante.emmanuel2017@gmail.com>
2024-12-07 09:56:20 +0800

    fix: move v-container from Applayout from register and log in views

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -7,34 +7,38 @@ import { useDisplay } from 'vuetify'
 const { mobile } = useDisplay()
 </script>
 <template>
-  <AppLayout
-    ><template #content>
-      <v-row>
-        <v-col class="mx-auto pt-16" cols="12" md="6">
-          <v-card class="mx-auto" elevation="20">
-            <v-card-title class="text-center">
-              <v-img
-                class="mx-auto my-5"
-                src="/suplink-logo.png"
-                :width="mobile ? '75%' : '25%'"
-              ></v-img>
-              <h3 class="font-weight-black">Welcome to SupportLink</h3>
-              <p>Login Form</p>
-            </v-card-title>
+  <AppLayout>
+    <template #content>
+      <v-container fluid>
+        <v-row>
+          <v-col class="mx-auto pt-16" cols="12" md="6">
+            <v-card class="mx-auto" elevation="20">
+              <v-card-title class="text-center">
+                <v-img
+                  class="mx-auto my-5"
+                  src="/suplink-logo.png"
+                  :width="mobile ? '75%' : '25%'"
+                ></v-img>
+                <h3 class="font-weight-black">Welcome to SupportLink</h3>
+                <p>Login Form</p>
+              </v-card-title>
 
-            <v-card-text class="bg-surface-light pt-4">
-              <LoginForm></LoginForm>
+              <v-card-text class="bg-surface-light pt-4">
+                <LoginForm></LoginForm>
 
-              <v-divider class="my-5"></v-divider>
+                <v-divider class="my-5"></v-divider>
 
-              <h5 class="text-center">
-                Don't have Account?
-                <RouterLink class="text-primary" to="/register">Click here to Register</RouterLink>
-              </h5>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+                <h5 class="text-center">
+                  Don't have Account?
+                  <RouterLink class="text-primary" to="/register"
+                    >Click here to Register</RouterLink
+                  >
+                </h5>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </template>
   </AppLayout>
 </template>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -9,32 +9,34 @@ const { mobile } = useDisplay()
 <template>
   <AppLayout>
     <template #content>
-      <v-row>
-        <v-col class="mx-auto pt-16" cols="12" md="6">
-          <v-card class="mx-auto" elevation="20">
-            <v-card-title class="text-center">
-              <v-img
-                class="mx-auto my-5"
-                src="/suplink-logo.png"
-                :width="mobile ? '75%' : '25%'"
-              ></v-img>
-              <h3 class="font-weight-black">Welcome to SupportLink</h3>
-              <p>Registration Form</p>
-            </v-card-title>
+      <v-container fluid>
+        <v-row>
+          <v-col class="mx-auto pt-16" cols="12" md="6">
+            <v-card class="mx-auto" elevation="20">
+              <v-card-title class="text-center">
+                <v-img
+                  class="mx-auto my-5"
+                  src="/suplink-logo.png"
+                  :width="mobile ? '75%' : '25%'"
+                ></v-img>
+                <h3 class="font-weight-black">Welcome to SupportLink</h3>
+                <p>Registration Form</p>
+              </v-card-title>
 
-            <v-card-text class="bg-surface-light pt-4">
-              <RegisterForm></RegisterForm>
+              <v-card-text class="bg-surface-light pt-4">
+                <RegisterForm></RegisterForm>
 
-              <v-divider class="my-5"></v-divider>
+                <v-divider class="my-5"></v-divider>
 
-              <h5 class="text-center">
-                Already have Account?
-                <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
-              </h5>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+                <h5 class="text-center">
+                  Already have Account?
+                  <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
+                </h5>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </template>
   </AppLayout>
 </template>
